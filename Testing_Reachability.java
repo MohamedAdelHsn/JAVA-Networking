f9d47c08da61e1843f99461e8772b33f231e0683
Mohamed Adel Hassan <58120325+MohamedAdelHsn@users.noreply.github.com>
2020-07-05 20:38:06 +0200

    Create Testing_Reachability.java

---
+++ Testing_Reachability.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.net.InetAddress;
+
+public class TestingReachability {
+
+	public static void main(String[] args) throws IOException {
+		
+		String url = "www.yahoo.com";
+		// get all ip addresses of this url
+		InetAddress[] addresses = InetAddress.getAllByName(url);
+		
+		for(InetAddress inet : addresses)
+		{
+			// ping all ip of this server  
+			if(inet.isReachable(10000)) 
+			{
+				 System.out.println(inet + " is reachable"); 
+				
+			}
+			else 
+			{
+				
+				 System.out.println(inet + " is not reachable");
+				
+			}
+			
+		}
+				
+
+	}
+
+}
